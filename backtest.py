fa873a34dc6909c9d63777180afc792a567163b3
arms22 <arms22@gmail.com>
2018-04-12 05:52:03 +0900

    update backtest.py

--- backtest.py
+++ backtest.py
@@ -1,1 +1,95 @@
 # -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+from numba import jit
+
+# PythonでFXシストレのバックテスト(1)
+# https://qiita.com/toyolab/items/e8292d2f051a88517cb2 より
+
+def Backtest(ohlc, BuyEntry, SellEntry, BuyExit, SellExit, lots=0.1, spread=2, fee=0):
+    Open = ohlc['open'].values #始値
+    N = len(ohlc) #FXデータのサイズ
+    BuyExit[N-2] = SellExit[N-2] = True #最後に強制エグジット
+    BuyPrice = SellPrice = 0.0 # 売買価格
+
+    LongTrade = np.zeros(N) # 買いトレード情報
+    ShortTrade = np.zeros(N) # 売りトレード情報
+
+    LongPL = np.zeros(N) # 買いポジションの損益
+    ShortPL = np.zeros(N) # 売りポジションの損益
+
+    for i in range(1,N):
+        if BuyEntry[i-1] and BuyPrice == 0: #買いエントリーシグナル
+            BuyPrice = Open[i] + spread
+            LongTrade[i] = BuyPrice #買いポジションオープン
+        elif BuyExit[i-1] and BuyPrice != 0: #買いエグジットシグナル
+            ClosePrice = Open[i]
+            LongTrade[i] = -ClosePrice #買いポジションクローズ
+            LongPL[i] = (ClosePrice - BuyPrice) * lots #損益確定
+            BuyPrice = 0
+
+        if SellEntry[i-1] and SellPrice == 0: #売りエントリーシグナル
+            SellPrice = Open[i]
+            ShortTrade[i] = SellPrice #売りポジションオープン
+        elif SellExit[i-1] and SellPrice != 0: #売りエグジットシグナル
+            ClosePrice = Open[i] + spread
+            ShortTrade[i] = -ClosePrice #売りポジションクローズ
+            ShortPL[i] = (SellPrice - ClosePrice) * lots #損益確定
+            SellPrice = 0
+
+    return pd.DataFrame({'Long':LongTrade, 'Short':ShortTrade}, index=ohlc.index),\
+            pd.DataFrame({'Long':LongPL, 'Short':ShortPL}, index=ohlc.index)
+
+
+def BacktestReport(Trade, PL):
+    LongPL = PL['Long']
+    LongTrades = np.count_nonzero(Trade['Long'])//2
+    LongWinTrades = np.count_nonzero(LongPL.clip_lower(0))
+    LongLoseTrades = np.count_nonzero(LongPL.clip_upper(0))
+    print('買いトレード数 =', LongTrades)
+    print('勝トレード数 =', LongWinTrades)
+    print('最大勝トレード =', LongPL.max())
+    print('平均勝トレード =', round(LongPL.clip_lower(0).sum()/LongWinTrades if LongWinTrades else 0, 2))
+    print('負トレード数 =', LongLoseTrades)
+    print('最大負トレード =', LongPL.min())
+    print('平均負トレード =', round(LongPL.clip_upper(0).sum()/LongLoseTrades if LongLoseTrades else 0, 2))
+    print('勝率 =', round(LongWinTrades/LongTrades*100, 2) if LongTrades else 0, '%\n')
+
+    ShortPL = PL['Short']
+    ShortTrades = np.count_nonzero(Trade['Short'])//2
+    ShortWinTrades = np.count_nonzero(ShortPL.clip_lower(0))
+    ShortLoseTrades = np.count_nonzero(ShortPL.clip_upper(0))
+    print('売りトレード数 =', ShortTrades)
+    print('勝トレード数 =', ShortWinTrades)
+    print('最大勝トレード =', ShortPL.max())
+    print('平均勝トレード =', round(ShortPL.clip_lower(0).sum()/ShortWinTrades, 2) if ShortWinTrades else 0)
+    print('負トレード数 =', ShortLoseTrades)
+    print('最大負トレード =', ShortPL.min())
+    print('平均負トレード =', round(ShortPL.clip_upper(0).sum()/ShortLoseTrades, 2) if ShortLoseTrades else 0)
+    print('勝率 =', round(ShortWinTrades/ShortTrades*100, 2) if ShortTrades else 0, '%\n')
+
+    Trades = LongTrades + ShortTrades
+    WinTrades = LongWinTrades+ShortWinTrades
+    LoseTrades = LongLoseTrades+ShortLoseTrades
+    print('総トレード数 =', Trades)
+    print('勝トレード数 =', WinTrades)
+    print('最大勝トレード =', max(LongPL.max(), ShortPL.max()))
+    print('平均勝トレード =', round((LongPL.clip_lower(0).sum()+ShortPL.clip_lower(0).sum())/WinTrades if WinTrades else 0, 2))
+    print('負トレード数 =', LoseTrades)
+    print('最大負トレード =', min(LongPL.min(), ShortPL.min()))
+    print('平均負トレード =', round((LongPL.clip_upper(0).sum()+ShortPL.clip_upper(0).sum())/LoseTrades if LoseTrades else 0, 2))
+    print('勝率 =', round(WinTrades/Trades*100, 2) if Trades else 0, '%\n')
+
+    GrossProfit = LongPL.clip_lower(0).sum()+ShortPL.clip_lower(0).sum()
+    GrossLoss = LongPL.clip_upper(0).sum()+ShortPL.clip_upper(0).sum()
+    Profit = GrossProfit+GrossLoss
+    Equity = (LongPL+ShortPL).cumsum()
+    MDD = (Equity.cummax()-Equity).max()
+    print('総利益 =', round(GrossProfit, 2))
+    print('総損失 =', round(GrossLoss, 2))
+    print('総損益 =', round(Profit, 2))
+    print('プロフィットファクター =', round(-GrossProfit/GrossLoss, 2))
+    print('平均損益 =', round(Profit/Trades, 2))
+    print('最大ドローダウン =', round(MDD, 2))
+    print('リカバリーファクター =', round(Profit/MDD, 2))
+    return Equity
--- channel_breakout_backtest.py
+++ channel_breakout_backtest.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
+from backtest import Backtest, BacktestReport
 
 def highest(series, window):
     return series.rolling(window).max()
@@ -8,13 +9,13 @@ def lowest(series, window):
 	return series.rolling(window).min()
 
 # テストデータ読み込み
-data = pd.read_csv('bitmex_20180410_5m.csv',
-	names=('timestamp','open','high','low','close', 'trades', 'volume', 'vwap'),
+data = pd.read_csv('bitmex_20180401_5m.csv',
+	names=('timestamp','close','foreignNotional','high','homeNotional','lastSize','low','open','symbol','trades','turnover','volume','vwap'),
 	index_col='timestamp',
 	parse_dates=True)
 
 # インジケーター作成
-breakout_in = 22
+breakout_in = 11
 breakout_out = 5
 
 long_entry_price = highest(data.high, breakout_in)
@@ -29,8 +30,16 @@ short_entry = data.close < short_entry_price.shift(1)
 long_exit = data.close < long_exit_price.shift(1)
 short_exit = data.close > short_exit_price.shift(1)
 
+long_entry[:breakout_in] = False
+short_entry[:breakout_in] = False
+long_exit[:breakout_out] = False
+short_exit[:breakout_out] = False
+
 # バックテスト実施
-entry_exit = pd.DataFrame({'open':data.open,
-	'long_entry_price':long_entry_price, 'long_exit_price':long_exit_price, 'long_entry':long_entry, 'long_exit':long_exit,
-	'short_entry_price':short_entry_price, 'short_entry':short_entry, 'short_exit_price':short_exit_price, 'short_exit':short_exit}, index=data.index)
-print(entry_exit.to_csv())
+# entry_exit = pd.DataFrame({'close':data.close, 'open':data.open,
+# 	'long_entry_price':long_entry_price, 'long_exit_price':long_exit_price, 'long_entry':long_entry, 'long_exit':long_exit,
+# 	'short_entry_price':short_entry_price, 'short_entry':short_entry, 'short_exit_price':short_exit_price, 'short_exit':short_exit}, index=data.index)
+# print(entry_exit.to_csv())
+
+(Trades, PL) = Backtest(data, long_entry, long_exit, short_entry, short_exit, lots=1)
+Equity = BacktestReport(Trades, PL)
