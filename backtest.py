715846e3864170ccf86fc058eeb84ee0ce625545
arms22 <arms22@gmail.com>
2018-04-13 17:51:41 +0900

    up-to-date

--- backtest.py
+++ backtest.py
@@ -2,12 +2,22 @@
 import numpy as np
 import pandas as pd
 from numba import jit
+from utils import dotdict
 
 # PythonでFXシストレのバックテスト(1)
 # https://qiita.com/toyolab/items/e8292d2f051a88517cb2 より
 @jit
-def Backtest(ohlc, buy_entry, sell_entry, buy_exit, sell_exit, lots=0.1, spread=2, fee=0):
-    Open = ohlc['open'].values #始値
+def Backtest(ohlc, buy_entry, sell_entry, buy_exit, sell_exit, lots=0.1, spread=0, take_profit=0, stop_loss=0):
+    Open = ohlc.open.values #始値
+    Low = ohlc.low.values #安値
+    High = ohlc.high.values #高値
+    Close = ohlc.close.values #始値
+
+    buy_entry = buy_entry.values
+    sell_entry = sell_entry.values
+    buy_exit = buy_exit.values
+    sell_exit = sell_exit.values
+
     N = len(ohlc) #データサイズ
     buy_exit[N-2] = sell_exit[N-2] = True #最後に強制エグジット
     BuyPrice = SellPrice = 0.0 # 売買価格
@@ -18,11 +28,16 @@ def Backtest(ohlc, buy_entry, sell_entry, buy_exit, sell_exit, lots=0.1, spread=
     LongPL = np.zeros(N) # 買いポジションの損益
     ShortPL = np.zeros(N) # 売りポジションの損益
 
+    #
+    # シグナルが出た次の足の始値で成行売買を行う
+    #
     for i in range(1, N):
+        # エントリー／イグジット
         if buy_entry[i-1] and BuyPrice == 0: #買いエントリーシグナル
             BuyPrice = Open[i] + spread
             LongTrade[i] = BuyPrice #買いポジションオープン
-        elif buy_exit[i-1] and BuyPrice != 0: #買いエグジットシグナル
+
+        if buy_exit[i-1] and BuyPrice != 0: #買いエグジットシグナル
             ClosePrice = Open[i]
             LongTrade[i] = -ClosePrice #買いポジションクローズ
             LongPL[i] = (ClosePrice - BuyPrice) * lots #損益確定
@@ -31,65 +46,147 @@ def Backtest(ohlc, buy_entry, sell_entry, buy_exit, sell_exit, lots=0.1, spread=
         if sell_entry[i-1] and SellPrice == 0: #売りエントリーシグナル
             SellPrice = Open[i]
             ShortTrade[i] = SellPrice #売りポジションオープン
-        elif sell_exit[i-1] and SellPrice != 0: #売りエグジットシグナル
+
+        if sell_exit[i-1] and SellPrice != 0: #売りエグジットシグナル
             ClosePrice = Open[i] + spread
             ShortTrade[i] = -ClosePrice #売りポジションクローズ
             ShortPL[i] = (SellPrice - ClosePrice) * lots #損益確定
             SellPrice = 0
 
-    return pd.DataFrame({'Long':LongTrade, 'Short':ShortTrade}, index=ohlc.index),\
-            pd.DataFrame({'Long':LongPL, 'Short':ShortPL}, index=ohlc.index)
+        # 利確 or 損切によるポジションの決済
+        if BuyPrice != 0:
+            ClosePrice = 0
+            if stop_loss > 0:
+                # 損切判定 Open -> Low
+                StopPrice = BuyPrice - stop_loss
+                if Open[i] <= StopPrice:
+                    ClosePrice = Open[i]
+                elif Low[i] <= StopPrice:
+                    ClosePrice = Low[i]
+            elif take_profit > 0:
+                # 利確判定
+                LimitPrice = BuyPrice + take_profit
+                if High[i] >= LimitPrice:
+                    ClosePrice = LimitPrice
+            if ClosePrice > 0:
+                LongTrade[i] = -ClosePrice #買いポジションクローズ
+                LongPL[i] = (ClosePrice - BuyPrice) * lots #損益確定
+                BuyPrice = 0
+
+        if SellPrice != 0:
+            ClosePrice = 0
+            if stop_loss > 0:
+                # 損切判定 Open -> High
+                StopPrice = SellPrice + stop_loss
+                if Open[i] >= StopPrice:
+                    ClosePrice = Open[i]
+                elif High[i] >= StopPrice:
+                    ClosePrice = High[i]
+            elif take_profit > 0:
+                # 利確判定
+                LimitPrice = SellPrice - take_profit
+                if Low[i] <= LimitPrice:
+                    ClosePrice = LimitPrice
+            if ClosePrice > 0:
+                ShortTrade[i] = -ClosePrice #売りポジションクローズ
+                ShortPL[i] = (SellPrice - ClosePrice) * lots #損益確定
+                SellPrice = 0
 
 
-def BacktestReport(Trade, PL):
-    LongPL = PL['Long']
-    LongTrades = np.count_nonzero(Trade['Long'])//2
-    LongWinTrades = np.count_nonzero(LongPL.clip_lower(0))
-    LongLoseTrades = np.count_nonzero(LongPL.clip_upper(0))
-    print('買いトレード数 =', LongTrades)
-    print('勝トレード数 =', LongWinTrades)
-    print('最大勝トレード =', LongPL.max())
-    print('平均勝トレード =', round(LongPL.clip_lower(0).sum()/LongWinTrades if LongWinTrades else 0, 2))
-    print('負トレード数 =', LongLoseTrades)
-    print('最大負トレード =', LongPL.min())
-    print('平均負トレード =', round(LongPL.clip_upper(0).sum()/LongLoseTrades if LongLoseTrades else 0, 2))
-    print('勝率 =', round(LongWinTrades/LongTrades*100, 2) if LongTrades else 0, '%\n')
+    return BacktestReport(
+        Trades=pd.DataFrame({'Long':LongTrade, 'Short':ShortTrade}, index=ohlc.index),
+        PL=pd.DataFrame({'Long':LongPL, 'Short':ShortPL}, index=ohlc.index))
 
-    ShortPL = PL['Short']
-    ShortTrades = np.count_nonzero(Trade['Short'])//2
-    ShortWinTrades = np.count_nonzero(ShortPL.clip_lower(0))
-    ShortLoseTrades = np.count_nonzero(ShortPL.clip_upper(0))
-    print('売りトレード数 =', ShortTrades)
-    print('勝トレード数 =', ShortWinTrades)
-    print('最大勝トレード =', ShortPL.max())
-    print('平均勝トレード =', round(ShortPL.clip_lower(0).sum()/ShortWinTrades, 2) if ShortWinTrades else 0)
-    print('負トレード数 =', ShortLoseTrades)
-    print('最大負トレード =', ShortPL.min())
-    print('平均負トレード =', round(ShortPL.clip_upper(0).sum()/ShortLoseTrades, 2) if ShortLoseTrades else 0)
-    print('勝率 =', round(ShortWinTrades/ShortTrades*100, 2) if ShortTrades else 0, '%\n')
 
-    Trades = LongTrades + ShortTrades
-    WinTrades = LongWinTrades+ShortWinTrades
-    LoseTrades = LongLoseTrades+ShortLoseTrades
-    print('総トレード数 =', Trades)
-    print('勝トレード数 =', WinTrades)
-    print('最大勝トレード =', max(LongPL.max(), ShortPL.max()))
-    print('平均勝トレード =', round((LongPL.clip_lower(0).sum()+ShortPL.clip_lower(0).sum())/WinTrades if WinTrades else 0, 2))
-    print('負トレード数 =', LoseTrades)
-    print('最大負トレード =', min(LongPL.min(), ShortPL.min()))
-    print('平均負トレード =', round((LongPL.clip_upper(0).sum()+ShortPL.clip_upper(0).sum())/LoseTrades if LoseTrades else 0, 2))
-    print('勝率 =', round(WinTrades/Trades*100, 2) if Trades else 0, '%\n')
+class BacktestReport:
+    def __init__(self, Trades, PL):
+        self.Long = dotdict()
+        self.Short = dotdict()
 
-    GrossProfit = LongPL.clip_lower(0).sum()+ShortPL.clip_lower(0).sum()
-    GrossLoss = LongPL.clip_upper(0).sum()+ShortPL.clip_upper(0).sum()
-    Profit = GrossProfit+GrossLoss
-    Equity = (LongPL+ShortPL).cumsum()
-    MDD = (Equity.cummax()-Equity).max()
-    print('総利益 =', round(GrossProfit, 2))
-    print('総損失 =', round(GrossLoss, 2))
-    print('総損益 =', round(Profit, 2))
-    print('プロフィットファクター =', round(-GrossProfit/GrossLoss, 2))
-    print('平均損益 =', round(Profit/Trades, 2))
-    print('最大ドローダウン =', round(MDD, 2))
-    print('リカバリーファクター =', round(Profit/MDD, 2))
-    return Equity
+        # ロング統計
+        LongPL = PL['Long']
+        self.Long.Trades = np.count_nonzero(LongPL)
+        self.Long.GrossProfit = LongPL.clip_lower(0).sum()
+        self.Long.GrossLoss =  LongPL.clip_upper(0).sum()
+        self.Long.Profit = self.Long.GrossProfit + self.Long.GrossLoss
+        self.Long.WinTrades = np.count_nonzero(LongPL.clip_lower(0))
+        self.Long.WinMax = LongPL.max()
+        if self.Long.WinTrades > 0:
+            self.Long.WinAverage = self.Long.GrossProfit / self.Long.WinTrades
+            self.Long.WinRatio = self.Long.WinTrades / self.Long.Trades
+        else:
+            self.Long.WinAverage = 0.0
+            self.Long.WinRatio = 0.0
+        self.Long.LossTrades = np.count_nonzero(LongPL.clip_upper(0))
+        self.Long.LossMax = LongPL.min()
+        if self.Long.LossTrades > 0:
+            self.Long.LossAverage = self.Long.GrossLoss / self.Long.LossTrades
+
+        # ショート統計
+        ShortPL = PL['Short']
+        self.Short.Trades = np.count_nonzero(ShortPL)
+        self.Short.GrossProfit = ShortPL.clip_lower(0).sum()
+        self.Short.GrossLoss = ShortPL.clip_upper(0).sum()
+        self.Short.Profit = self.Short.GrossProfit + self.Short.GrossLoss
+        self.Short.WinTrades = np.count_nonzero(ShortPL.clip_lower(0))
+        self.Short.WinMax = ShortPL.max()
+        if self.Short.WinTrades > 0:
+            self.Short.WinAverage = self.Short.GrossProfit / self.Short.WinTrades
+            self.Short.WinRatio = self.Short.WinTrades / self.Short.Trades
+        else:
+            self.Short.WinAverage = 0.0
+            self.Short.WinRatio = 0.0
+        self.Short.LossTrades = np.count_nonzero(ShortPL.clip_upper(0))
+        self.Short.LossMax = ShortPL.min()
+        if self.Short.LossTrades > 0:
+            self.Short.LossAverage = self.Short.GrossLoss / self.Short.LossTrades
+
+        # 全体統計
+        self.Trades = self.Long.Trades + self.Short.Trades
+        self.WinTrades = self.Long.WinTrades + self.Short.WinTrades
+        self.WinRatio = self.WinTrades / self.Trades if self.Trades > 0 else 0.0
+        self.LossTrades = self.Long.LossTrades + self.Short.LossTrades
+        self.GrossProfit = self.Long.GrossProfit + self.Short.GrossProfit
+        self.GrossLoss = self.Long.GrossLoss + self.Short.GrossLoss
+        self.Profit = self.GrossProfit + self.GrossLoss
+        self.Equity = (LongPL + ShortPL).cumsum()
+        self.DrawDown = (self.Equity.cummax() - self.Equity).max()
+        self.ProfitFactor = self.GrossProfit / -self.GrossLoss if -self.GrossLoss > 0 else 0.0
+        self.RecoveryFactor = self.ProfitFactor / self.DrawDown if self.DrawDown > 0 else 0.0
+
+    def __str__(self):
+        return 'Long\n' \
+        '  Trades :' + str(self.Long.Trades) + '\n' \
+        '  WinTrades :' + str(self.Long.WinTrades) + '\n' \
+        '  WinMax :' + str(self.Long.WinMax) + '\n' \
+        '  WinAverage :' + str(self.Long.WinAverage) + '\n' \
+        '  WinRatio :' + str(self.Long.WinRatio) + '\n' \
+        '  LossTrades :' + str(self.Long.LossTrades) + '\n' \
+        '  LossMax :' + str(self.Long.LossMax) + '\n' \
+        '  LossAverage :' + str(self.Long.LossAverage) + '\n' \
+        '  GrossProfit :' + str(self.Long.GrossProfit) + '\n' \
+        '  GrossLoss :' + str(self.Long.GrossLoss) + '\n' \
+        '  Profit :' + str(self.Long.Profit) + '\n' \
+        '\nShort\n' \
+        '  Trades :' + str(self.Short.Trades) + '\n' \
+        '  WinTrades :' + str(self.Short.WinTrades) + '\n' \
+        '  WinMax :' + str(self.Short.WinMax) + '\n' \
+        '  WinAverage :' + str(self.Short.WinAverage) + '\n' \
+        '  WinRatio :' + str(self.Short.WinRatio) + '\n' \
+        '  LossTrades :' + str(self.Short.LossTrades) + '\n' \
+        '  LossMax :' + str(self.Short.LossMax) + '\n' \
+        '  LossAverage :' + str(self.Short.LossAverage) + '\n' \
+        '  GrossProfit :' + str(self.Short.GrossProfit) + '\n' \
+        '  GrossLoss :' + str(self.Short.GrossLoss) + '\n' \
+        '  Profit :' + str(self.Short.Profit) + '\n' \
+        '\nTotal\n' \
+        '  Trades :' + str(self.Trades) + '\n' \
+        '  WinTrades :' + str(self.WinTrades) + '\n' \
+        '  WinRatio :' + str(self.WinRatio) + '\n' \
+        '  LossTrades :' + str(self.LossTrades) + '\n' \
+        '  GrossProfit :' + str(self.GrossProfit) + '\n' \
+        '  GrossLoss :' + str(self.GrossLoss) + '\n' \
+        '  Profit :' + str(self.Profit) + '\n' \
+        '  DrawDown :' + str(self.DrawDown) + '\n' \
+        '  ProfitFactor :' + str(self.ProfitFactor) + '\n' \
+        '  RecoveryFactor :' + str(self.RecoveryFactor) + '\n'
--- channel_breakout_backtest.py
+++ channel_breakout_backtest.py
@@ -12,22 +12,23 @@ def lowest(series, window):
     return series.rolling(window).min()
 
 # テストデータ読み込み
-data = pd.read_csv('bitmex_201804_5m.csv', index_col='timestamp', parse_dates=True)
+data = pd.read_csv('bitmex_201804_1m.csv', index_col='timestamp', parse_dates=True)
 #print(data.head())
 
 @jit
-def channel_breakout_backtest(ohlc, breakout_in, breakout_out):
+def channel_breakout_backtest(ohlc, breakout_in, breakout_out, take_profit=0, stop_loss=0):
     # インジケーター作成
     long_entry_price = highest(ohlc.high, breakout_in)
-    short_entry_price = lowest(ohlc.low, breakout_in)
-
     long_exit_price = lowest(ohlc.low, breakout_out)
+
+    short_entry_price = lowest(ohlc.low, breakout_in)
     short_exit_price = highest(ohlc.high, breakout_out)
 
     # エントリー／イグジット
     long_entry = ohlc.close > long_entry_price.shift(1)
-    short_entry = ohlc.close < short_entry_price.shift(1)
     long_exit = ohlc.close < long_exit_price.shift(1)
+
+    short_entry = ohlc.close < short_entry_price.shift(1)
     short_exit = ohlc.close > short_exit_price.shift(1)
 
     long_entry[:breakout_in] = False
@@ -41,46 +42,30 @@ def channel_breakout_backtest(ohlc, breakout_in, breakout_out):
     #     'short_entry_price':short_entry_price, 'short_entry':short_entry, 'short_exit_price':short_exit_price, 'short_exit':short_exit}, index=data.index)
     # print(entry_exit.to_csv())
 
-    return Backtest(data, buy_entry=long_entry, sell_entry=short_entry, buy_exit=long_exit, sell_exit=short_exit, lots=1)
-
+    return Backtest(data, buy_entry=long_entry, sell_entry=short_entry, buy_exit=long_exit, sell_exit=short_exit, lots=1, spread=0.5, take_profit=take_profit, stop_loss=stop_loss)
 
 # 参考
 # https://qiita.com/kenchin110100/items/ac3edb480d789481f134
+
+take_profit = 0
+stop_loss = 40
 
 def objective(args):
     breakout_in = int(args['breakout_in'])
     breakout_out = int(args['breakout_out'])
 
-    if breakout_in < breakout_out:
-        return 10000
+    # if breakout_in < breakout_out:
+    #     return 10000
 
-    (Trades, PL) = channel_breakout_backtest(data, breakout_in, breakout_out)
+    report = channel_breakout_backtest(data, breakout_in, breakout_out, take_profit, stop_loss)
 
-    LongPL = PL['Long']
-    LongTrades = np.count_nonzero(Trades['Long'])//2
-    LongWinTrades = np.count_nonzero(LongPL.clip_lower(0))
-    LongLoseTrades = np.count_nonzero(LongPL.clip_upper(0))
-
-    ShortPL = PL['Short']
-    ShortTrades = np.count_nonzero(Trades['Short'])//2
-    ShortWinTrades = np.count_nonzero(ShortPL.clip_lower(0))
-    ShortLoseTrades = np.count_nonzero(ShortPL.clip_upper(0))
-
-    Trades = LongTrades + ShortTrades
-    WinTrades = LongWinTrades+ShortWinTrades
-    LoseTrades = LongLoseTrades+ShortLoseTrades
-
-    GrossProfit = LongPL.clip_lower(0).sum() + ShortPL.clip_lower(0).sum()
-    GrossLoss = LongPL.clip_upper(0).sum() + ShortPL.clip_upper(0).sum()
-    ProfitFactor = -(GrossProfit / GrossLoss)
-
-    print(breakout_in, ',', breakout_out, ',', ProfitFactor, ',', GrossProfit+GrossLoss, ',', GrossProfit, ',', GrossLoss, ',', Trades, ',', WinTrades, ',', LoseTrades, ',', WinTrades/Trades)
-    return -1 * ProfitFactor
+    print(breakout_in, ',', breakout_out, ',', take_profit, ',', stop_loss, ',', report.ProfitFactor, ',', report.Profit, ',', report.GrossProfit, ',', report.GrossLoss, ',', report.Trades, ',', report.WinTrades, ',', report.LossTrades, ',', report.WinRatio)
+    return -1 * report.ProfitFactor
 
 # 探索するパラメータ
 hyperopt_parameters = {
-    'breakout_in': hp.quniform('breakout_in', 5, 200, 2), # 
-    'breakout_out': hp.quniform('breakout_out', 5, 200, 2),
+    'breakout_in': hp.quniform('breakout_in', 5, 100, 2),
+    'breakout_out': hp.quniform('breakout_out', 5, 100, 2),
 }
 
 # iterationする回数
@@ -108,6 +93,5 @@ best = fmin(
 
 print('best:', best)
 
-(Trades, PL) = channel_breakout_backtest(data, int(best['breakout_in']), int(best['breakout_out']))
-Equity = BacktestReport(Trades, PL)
-#print(Equity.to_csv())
+report = channel_breakout_backtest(data, int(best['breakout_in']), int(best['breakout_out']), take_profit, stop_loss)
+print(report)
