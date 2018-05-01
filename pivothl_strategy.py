17781130bb9a982e425570c79f7eb9127a76e996
arms22 <arms22@gmail.com>
2018-05-01 14:07:36 +0000

    update pivothl_strategy.py

--- pivothl_strategy.py
+++ pivothl_strategy.py
@@ -17,21 +17,21 @@ def pivot_highlow_strategy(ticker, ohlcv, position, balance, strategy):
     short_entry_price = last(pivotlow(ohlcv.low, leftbars, rightbars).ffill())
 
 	# ロット数計算
-    qty_lot = int(balance.BTC.free * 0.1 * ticker.last)
+    qty_lot = int(balance.BTC.free * 0.05 * ticker.last)
 
     # 最大ポジション数設定
     strategy.risk.max_position_size = qty_lot
 
     # 注文（ポジションがある場合ドテン）
-    strategy.entry('L', 'buy', qty=qty_lot, limit=max(long_entry_price, ticker.ask), stop=long_entry_price+0.5)
-    strategy.entry('S', 'sell', qty=qty_lot, limit=min(short_entry_price, ticker.bid), stop=short_entry_price-0.5)
+    strategy.entry('L', 'buy', qty=qty_lot, limit=max(long_entry_price, ticker.bid), stop=long_entry_price+0.5)
+    strategy.entry('S', 'sell', qty=qty_lot, limit=min(short_entry_price, ticker.ask), stop=short_entry_price-0.5)
 
 strategy = Strategy(pivot_highlow_strategy)
 strategy.settings.timeframe = '1m'
 strategy.settings.interval = 5
 strategy.settings.apiKey = settings.apiKey
 strategy.settings.secret = settings.secret
-strategy.testnet.use = False
+strategy.testnet.use = True
 strategy.testnet.apiKey = settings.testnet_apiKey
 strategy.testnet.secret = settings.testnet_secret
 strategy.start()
