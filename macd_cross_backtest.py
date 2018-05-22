8e3b4a16a85bec756fa5f966f5aad1b1bef2fe45
arms22 <arms22@gmail.com>
2018-05-22 17:54:59 +0900

    add start/end option for macd backtest

--- macd_cross_backtest.py
+++ macd_cross_backtest.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import argparse
+import datetime
+import dateutil.parser
 import pandas as pd
 import numpy as np
 from datetime import datetime
@@ -10,6 +13,14 @@ from functools import lru_cache
 from functools import wraps
 import redis
 import pickle
+
+def store_datetime(str):
+    return dateutil.parser.parse(str)
+
+parser = argparse.ArgumentParser(description="")
+parser.add_argument("--start", type=store_datetime)
+parser.add_argument("--end", type=store_datetime)
+args = parser.parse_args()
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
@@ -34,12 +45,19 @@ def read_ohlc(filename):
 
 ohlcv = read_ohlc('csv/bitmex_2018_1h.csv')
 
-@lru_cache(maxsize=32)
+if args.start is not None and args.end is not None:
+    ohlcv = ohlcv[args.start:args.end]
+elif args.start is None:
+    ohlcv = ohlcv[:args.end]
+elif args.end is None:
+    ohlcv = ohlcv[args.start:]
+
+@lru_cache(maxsize=None)
 #@redis_cache(r)
 def cached_sma(period):
     return ohlcv.close.rolling(int(period)).mean()
 
-@lru_cache(maxsize=32)
+@lru_cache(maxsize=None)
 #@redis_cache(r)
 def cached_macd(fastlen, slowlen, siglen):
     macd = cached_sma(fastlen) - cached_sma(slowlen)
@@ -78,31 +96,31 @@ def macd_cross_backtest(fastlen, slowlen, siglen, smafastlen, smaslowlen, use_sm
 
     lots = (1000 / ohlcv.close)
 
-    # entry_exit = pd.DataFrame({'close':ohlcv.close, 'macd':vmacd, 'sig':vsig,
-    #     'long_entry_price':long_entry_price, 'long_exit_price':long_exit_price, 'long_entry':long_entry, 'long_exit':long_exit,
-    #     'short_entry_price':short_entry_price, 'short_entry':short_entry, 'short_exit_price':short_exit_price, 'short_exit':short_exit})#, index=ohlcv.index)
-    # entry_exit.to_csv('entry_exit.csv')
+    entry_exit = pd.DataFrame({'close':ohlcv.close, 'macd':vmacd, 'sig':vsig, #'fast':vfast, 'slow':vslow,
+        'long_entry_price':long_entry_price, 'long_exit_price':long_exit_price, 'long_entry':long_entry, 'long_exit':long_exit,
+        'short_entry_price':short_entry_price, 'short_entry':short_entry, 'short_exit_price':short_exit_price, 'short_exit':short_exit})#, index=ohlcv.index)
+    entry_exit.to_csv('entry_exit.csv')
 
     report = Backtest(ohlcv, buy_entry=long_entry, sell_entry=short_entry, buy_exit=long_exit, sell_exit=short_exit,
         stop_buy_entry=long_entry_price, stop_sell_entry=short_entry_price, stop_buy_exit=long_exit_price, stop_sell_exit=short_exit_price,
-        lots=lots, spread=0, take_profit=0, stop_loss=0, trailing_stop=0, slippage=0, percent_of_equity=(1, 740))
+        lots=1, spread=0, take_profit=0, stop_loss=0, trailing_stop=0, slippage=0)#, percent_of_equity=(1, 1000))
     return report
 
 default_parameters = {
     'fastlen':19,
     'slowlen':27,
     'siglen':13,
-    'smafastlen':22,
-    'smaslowlen':26,
-    'use_sma':True,
+    'smafastlen':19,
+    'smaslowlen':27,
+    'use_sma':False,
 }
 
 hyperopt_parameters = {
-    'fastlen': hp.quniform('fastlen', 1, 50, 1),
-    'slowlen': hp.quniform('slowlen', 1, 50, 1),
+    'fastlen': hp.quniform('fastlen', 5, 50, 1),
+    'slowlen': hp.quniform('slowlen', 5, 50, 1),
     'siglen': hp.quniform('siglen', 1, 50, 1),
-    'smafastlen': hp.quniform('smafastlen', 1, 50, 1),
-    'smaslowlen': hp.quniform('smaslowlen', 1, 50, 1),
+    # 'smafastlen': hp.quniform('smafastlen', 1, 50, 1),
+    # 'smaslowlen': hp.quniform('smaslowlen', 1, 50, 1),
 }
 
-BacktestIteration(macd_cross_backtest, default_parameters, hyperopt_parameters, 1000)
+BacktestIteration(macd_cross_backtest, default_parameters, hyperopt_parameters, 0)
